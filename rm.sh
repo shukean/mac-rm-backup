612dc362d787764850eddf9005fd13f525ade151
yky <yky@yky.pw>
2016-02-14 16:39:41 +0800

    add judge copy result

--- rm.sh
+++ rm.sh
@@ -12,8 +12,12 @@ args=($*)
 for arg in $*; do
     _tmp=$curPath/$arg
     if [ -b $_tmp -o -c $_tmp -o -d $_tmp -o -f $_tmp ]; then
-        printf '%b\n' "\33[32m[✔]\33[0m copy $arg" >&2
         cp -r $_tmp $backPath
+        if [ "$?" -eq '0' ]; then
+            printf '%b\n' "\33[32m[✔]\33[0m copy $arg" >&2
+        else
+            printf '%b\n' "\33[31m[✘]\33[0m copy fail $arg"
+        fi
     fi
 done
 
